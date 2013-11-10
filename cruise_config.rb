10ce647e538cf7ff5ef206048d243b4be74cd932
Matthias Grosser <mtgrosser@gmx.net>
2013-11-10 22:29:25 +0100

    Update cruise config

--- cruise_config.rb
+++ cruise_config.rb
@@ -2,6 +2,6 @@
 
 Project.configure do |project|
   project.scheduler.polling_interval = 30.minutes
-  #project.use_bundler = false
-  #project.build_command = '/usr/local/bin/rbfu @2.0.0 ruby cruise_script.rb'
+  project.use_bundler = false
+  project.build_command = 'rruby cruise_script.rb'
 end
